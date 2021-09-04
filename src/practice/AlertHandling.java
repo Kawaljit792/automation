50eb22cb2eefa4c6d89f3b2e7d1ffd65e03f5893
kawaljit792 <kavikaurgill@gmail.com>
2021-09-04 15:26:04 +1000

    new changes

--- src/practice/AlertHandling.java
+++ src/practice/AlertHandling.java
@@ -2,7 +2,8 @@ package practice;
 
 import java.awt.AWTException;
 import java.util.concurrent.TimeUnit;
-
+import java.util.Iterator;
+import java.util.Set;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,8 +15,8 @@ public class AlertHandling {
 		
 		AlertHandling obj=new AlertHandling();
 		obj.invokeBrowser();
-		obj.alertFunctions();
-		
+		//obj.alertFunctions();
+		obj.windowFunctions();
 	}
 
 	WebDriver driver;
@@ -42,21 +43,44 @@ public class AlertHandling {
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//body/div[@id='easycont']/div[1]/div[2]/div[1]/div[2]/button[1]")).click();
 		Thread.sleep(3000);	
-		// for and alert box
+		// for alert box
 		Alert alert= driver.switchTo().alert();
 		String alertMessage= driver.switchTo().alert().getText();
 		System.out.println(alertMessage);
 		alert.accept();
+		//alert.dismiss(); to dismiss the alert
 		Thread.sleep(3000);
+		driver.close();
+	}
+	
+	public void windowFunctions() throws InterruptedException {
 		// for a pop-up window
 		driver.get("https://www.seleniumeasy.com/test/window-popup-modal-demo.html");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//a[contains(text(),'Follow On Twitter')]")).click();
 		Thread.sleep(3000);
-		driver.switchTo().window("2000");
-		Thread.sleep(3000);
-		driver.close();
 		
+		String parentWindow=driver.getWindowHandle(); //  will return parent window name as a string
+		Set<String>s=driver.getWindowHandles();
+
+		// Now iterate using Iterator
+		Iterator<String> It= s.iterator();
+
+		while(It.hasNext())
+		{
+
+		String child_window=It.next();
+
+
+		if(!parentWindow.equals(child_window))
+		{
+		driver.switchTo().window(child_window);
+		driver.close();
+		}
+		driver.switchTo().window(parentWindow);
+		Thread.sleep(2000);
+		driver.close();
+		}	
 		/* Robot robot= new Robot();
 		robot.mouseMove(1500,150); // to get to the close button of window
 		robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
@@ -66,13 +90,7 @@ public class AlertHandling {
 		 */
 		//driver.switchTo().window(alertMessage).close();
 		
-		Thread.sleep(2000);
-		driver.close();
-		
-		//driver.quit();
-		
-		
+		}	
 		
 	}
-	
-}
+
--- src/practice/PrimeNumber.java
+++ src/practice/PrimeNumber.java
@@ -6,22 +6,31 @@ public class PrimeNumber {
 
 		 Scanner s = new Scanner(System.in);  
 	       System.out.print("Enter a number : ");  
-	      
+	      int flag=0;
 	       int n = s.nextInt(); 
-	      if (n==0||n==1)
+	      if (n==0||n==1) 
+	     
 	    	  System.out.println(n+" is not a prime number");
-	      else
+	     else
 	      {
 	    	  for(int i=2; i<=(int)Math.sqrt(n);i++)
 	    	  {
 	    		  if(n%i==0)
 	    		  {
-	    			  System.out.println(n+" is not a prime number");
-	    			  break;
-	    			  }    		 
+	    			  flag=1;
+	    			  }   
+	    	    	    		  
 	    	  }
+	    	  if (flag==1)
+	    	      System.out.println(n+" is not a prime number");
+	    	      else
+	    	    	  System.out.println(n+" is a prime number");
+	    	     
 	      }
+	     
 	    s.close();   
+	    
+	    
 	}
 
 }
